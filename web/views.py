920e03ba0353d6129f97a4bb63737c9ea2048b6d
Arroyo Walter <arroyowalterd@gmail.com>
2023-03-14 15:30:42 -0300

    Agrega Django: view Home y Signup

---
+++ web/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+# Create your views here.
+
+def signup(request):    
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'title': 'SignUp',
+        'form': UserCreationForm
+    })
+
+    else:
+        if request.POST['password1'] ==  request.POST['password2']:
+            try:
+                #Register User
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('Password do not match')
+
+    
+
+def home(request):
+    return render(request, 'home.html')
+
