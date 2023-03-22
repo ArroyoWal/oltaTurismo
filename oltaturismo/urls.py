87fde0f0db47dd20ff23579b0b8124f6a2569537
Arroyo Walter <arroyowalterd@gmail.com>
2023-03-22 09:15:22 -0300

    agrega logout y signin

--- oltaturismo/urls.py
+++ oltaturismo/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
-    path('logout/', views.signout, name='loguot')
+    path('logout/', views.signout, name='loguot'),
+    path('signin/', views.signin, name='signin')
 ]
